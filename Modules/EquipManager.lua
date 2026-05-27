1394c2e5a2c1605f20f41c28c85655a383ce9408
Resist <rfb.resist@gmail.com>
2026-05-27 00:17:50 -0300

    fix(EquipManager): save slots/weapons not working properly

--- Modules/EquipManager.lua
+++ Modules/EquipManager.lua
@@ -1,69 +1,33 @@
 local _, Addon = ...
 
-SLASH_REPLUS1 = "/replus"
-SlashCmdList["REPLUS"] = function()
-	Settings.OpenToCategory(Addon.SettingsCategoryId)
+local em = C_EquipmentSet
 
-	DEFAULT_CHAT_FRAME:AddMessage("|cffffff00[Replus]|r help:")
-
-	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/showsets|r - Show all sets")
-	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/delset|r |cffffff00name|r - Delete a set")
-	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/saveset|r |cffffff00name|r - Save current gear as a set")
-	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/saveweapons|r |cffffff00name|r - Save current weapons as a set")
-	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/saveslots|r |cffffff00name|r |cffffff00slots|r - Save slots as a set")
-	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/equipset|r |cffffff00name|r - Equip a set")
-end
-
-SLASH_SHOWSETS1 = "/showsets"
-SLASH_SHOWSETS2 = "/getsets"
-SLASH_SHOWSETS3 = "/allsets"
-SlashCmdList["SHOWSETS"] = function()
-	DEFAULT_CHAT_FRAME:AddMessage("|cffffff00[Replus]|r showing all sets:")
-
-	for _, id in pairs(C_EquipmentSet.GetEquipmentSetIDs()) do
-		local name = C_EquipmentSet.GetEquipmentSetInfo(id)
-		DEFAULT_CHAT_FRAME:AddMessage("- |cff00ff00" .. name .. "|r")
-	end
-end
-
-SLASH_DELSET1 = "/delset"
-SlashCmdList["DELSET"] = function(set)
-	C_EquipmentSet.DeleteEquipmentSet(C_EquipmentSet.GetEquipmentSetID(set))
-
-	DEFAULT_CHAT_FRAME:AddMessage("|cffffff00[Replus]|r |cff00ff00" .. set .. "|r set deleted")
-end
-
-SLASH_SAVESET1 = "/saveset"
-SlashCmdList["SAVESET"] = function(set)
-	C_EquipmentSet.ClearIgnoredSlotsForSave()
-	C_EquipmentSet.CreateEquipmentSet(set)
-	C_EquipmentSet.IgnoreSlotForSave(INVSLOT_BODY) -- ignore shirt
-	C_EquipmentSet.IgnoreSlotForSave(INVSLOT_TABARD) -- ignore tabard
-	C_EquipmentSet.SaveEquipmentSet(C_EquipmentSet.GetEquipmentSetID(set))
-
-	DEFAULT_CHAT_FRAME:AddMessage("|cffffff00[Replus]|r |cff00ff00" .. set .. "|r set saved")
-end
+local REPLUS = "|cffffff00[Replus]|r"
+local SETNAME = "|cff00ff00%s|r"
+local SET_SAVED = "|cffffff00[Replus]|r |cff00ff00%s|r set saved"
+local SET_NOT_FOUND = "|cffffff00[Replus]|r ERROR: |cff00ff00%s|r set not found"
+local SET_DELETED = "|cffffff00[Replus]|r |cff00ff00%s|r set deleted"
+local SET_WEAPONS_SAVED = "|cffffff00[Replus]|r |cff00ff00%s|r weapons set saved"
+local WRONG_SLOT = "|cffffff00[Replus]|r ERROR: slot number '%s' not recognized"
 
 ---@param set string
 ---@param slots table
 local function saveSlots(set, slots)
-	C_EquipmentSet.ClearIgnoredSlotsForSave()
-	C_EquipmentSet.CreateEquipmentSet(set)
+	local id = em.GetEquipmentSetID(set)
+	if not id then
+		em.CreateEquipmentSet(set)
+		id = em.GetEquipmentSetID(set)
+	end
+
+	em.ClearIgnoredSlotsForSave()
+
 	for i = 1, 19 do
-		if slots[i] ~= true then
-			C_EquipmentSet.IgnoreSlotForSave(i)
+		if not slots[i] then
+			em.IgnoreSlotForSave(i)
 		end
 	end
-	C_EquipmentSet.SaveEquipmentSet(C_EquipmentSet.GetEquipmentSetID(set))
-end
 
-SLASH_SAVEWEAPONS1 = "/savew"
-SLASH_SAVEWEAPONS2 = "/savewp"
-SLASH_SAVEWEAPONS3 = "/saveweapon"
-SLASH_SAVEWEAPONS4 = "/saveweapons"
-SlashCmdList["SAVEWEAPONS"] = function(set)
-	saveSlots(set, { [16] = true, [17] = true })
-	DEFAULT_CHAT_FRAME:AddMessage("|cffffff00[Replus]|r |cff00ff00" .. set .. "|r weapons set saved")
+	em.SaveEquipmentSet(id)
 end
 
 ---@param opts string
@@ -79,7 +43,7 @@ local function parseSlots(opts)
 		else
 			local slot = tonumber(str)
 			if slot == nil or slot < 1 or slot > 19 then
-				DEFAULT_CHAT_FRAME:AddMessage("|cffffff00[Replus]|r slot number not recognized")
+				DEFAULT_CHAT_FRAME:AddMessage(WRONG_SLOT:format(str))
 				return nil, nil
 			end
 
@@ -90,6 +54,70 @@ local function parseSlots(opts)
 	return set, slots
 end
 
+SLASH_REPLUS1 = "/replus"
+SlashCmdList["REPLUS"] = function()
+	Settings.OpenToCategory(Addon.SettingsCategoryId)
+
+	DEFAULT_CHAT_FRAME:AddMessage(REPLUS .. " help:")
+
+	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/showsets|r - Show all sets")
+	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/delset|r |cffffff00name|r - Delete a set")
+	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/saveset|r |cffffff00name|r - Save current gear as a set")
+	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/saveweapons|r |cffffff00name|r - Save current weapons as a set")
+	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/saveslots|r |cffffff00name|r |cffffff00slots|r - Save slots as a set")
+	DEFAULT_CHAT_FRAME:AddMessage("- |cffff8000/equipset|r |cffffff00name|r - Equip a set")
+end
+
+SLASH_SHOWSETS1 = "/showsets"
+SLASH_SHOWSETS2 = "/getsets"
+SLASH_SHOWSETS3 = "/allsets"
+SlashCmdList["SHOWSETS"] = function()
+	DEFAULT_CHAT_FRAME:AddMessage(REPLUS .. "showing all sets:")
+
+	for _, id in pairs(em.GetEquipmentSetIDs()) do
+		local name = em.GetEquipmentSetInfo(id)
+		DEFAULT_CHAT_FRAME:AddMessage("- " .. SETNAME:format(name))
+	end
+end
+
+SLASH_DELSET1 = "/delset"
+SlashCmdList["DELSET"] = function(set)
+	local id = em.GetEquipmentSetID(set)
+	if not id then
+		DEFAULT_CHAT_FRAME:AddMessage(SET_NOT_FOUND:format(set))
+		return
+	end
+
+	em.DeleteEquipmentSet(id)
+	DEFAULT_CHAT_FRAME:AddMessage(SET_DELETED:format(set))
+end
+
+SLASH_SAVESET1 = "/saveset"
+SlashCmdList["SAVESET"] = function(set)
+	local id = em.GetEquipmentSetID(set)
+	if not id then
+		em.CreateEquipmentSet(set)
+		id = em.GetEquipmentSetID(set)
+	end
+
+	em.ClearIgnoredSlotsForSave()
+
+	em.IgnoreSlotForSave(INVSLOT_BODY) -- ignore shirt
+	em.IgnoreSlotForSave(INVSLOT_TABARD) -- ignore tabard
+	em.SaveEquipmentSet(id)
+
+	DEFAULT_CHAT_FRAME:AddMessage(SET_SAVED:format(set))
+end
+
+SLASH_SAVEWEAPONS1 = "/savew"
+SLASH_SAVEWEAPONS2 = "/savewp"
+SLASH_SAVEWEAPONS3 = "/saveweapon"
+SLASH_SAVEWEAPONS4 = "/saveweapons"
+SlashCmdList["SAVEWEAPONS"] = function(set)
+	saveSlots(set, { [16] = true, [17] = true })
+	DEFAULT_CHAT_FRAME:AddMessage(SET_WEAPONS_SAVED:format(set))
+end
+
 SLASH_SAVESLOTS1 = "/saveslots"
 SlashCmdList["SAVESLOTS"] = function(opts)
 	local set, slots = parseSlots(opts)
@@ -98,5 +126,5 @@ SlashCmdList["SAVESLOTS"] = function(opts)
 	end
 
 	saveSlots(set, slots)
-	DEFAULT_CHAT_FRAME:AddMessage("|cffffff00[Replus]|r |cff00ff00" .. set .. "|r set saved")
+	DEFAULT_CHAT_FRAME:AddMessage(SET_SAVED:format(set))
 end
